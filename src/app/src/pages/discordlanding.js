712c25837416e3dd68d573740aa174d87c5a9b3f
Abu Buwe <abubakarbuwe@gmail.com>
2022-02-27 10:28:16 +0000

    add new discord landing page

--- src/app/src/pages/discordlanding.js
+++ src/app/src/pages/discordlanding.js
@@ -1,6 +1,11 @@
 import { useRouter } from "next/router"
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../utils/UserContext";
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { VStack, Image, Button } from "@chakra-ui/react";
+import Head from 'next/head'
+
 
 export default function DiscordLanding(req, res) {
 
@@ -52,6 +57,7 @@ export default function DiscordLanding(req, res) {
     const tokenId = 0;
     const contractAddress = '0x47a2f25ad83Efa1BaA376D062284e777dD223463';
 
+    console.log(user)
     try {
       var res = await fetch('/api/mint', {
         method: 'POST',
@@ -73,8 +79,37 @@ export default function DiscordLanding(req, res) {
 
 
   return (
-    <>
-    <button onClick={handleClaim}>Claim OxHack NFT</button>
-    </>
+    <Container height="100vh">
+    <Head>
+      <title>Claim your OxHack NFT </title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Main>
+      <VStack>
+      <Image
+            objectFit={'cover'}
+            width={'100%'}
+            maxW={'400px'}
+            src={
+              'https://cdn.discordapp.com/attachments/945665299938697260/947382707309137980/OxHack.png'
+            }
+            layout={'fill'}
+          />
+      <Button
+        bg={'green.400'}
+        color={'white'}
+        m={'em !important'}
+        // w="full"
+        _hover={{
+          bg: 'blue.500',
+        }}
+        onClick={handleClaim}
+        >
+        Claim OxHack NFT
+      </Button>
+      </VStack>
+    </Main>
+    </Container>
   )
 }
