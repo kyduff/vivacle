825b2a5558ddaf37561e2eb5c6b5daf41f67233c
Abu Buwe <abubakarbuwe@gmail.com>
2022-02-27 13:54:21 +0000

    move provider into if statement to silence error

--- src/app/src/utils/metamask.ts
+++ src/app/src/utils/metamask.ts
@@ -3,27 +3,27 @@ import { ethers } from 'ethers';
 declare const window: any;
 
 export async function getAddress() {
-  const provider = new ethers.providers.Web3Provider(window.ethereum, "rinkeby");
   // Check if MetaMask installed
   if (window.ethereum) {
-      // check if correct network connected
-      const chainId = await window.ethereum.request({ method: 'eth_chainId'});
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "rinkeby");
+    // check if correct network connected
+    const chainId = await window.ethereum.request({ method: 'eth_chainId'});
 
-      console.log(chainId)
-      if ( chainId != "0x4") {
-          alert("Please change to Rinkeby/")
-      }
-      else {
-          
-          await provider.send("eth_requestAccounts", []);
-          const signer = provider.getSigner();
+    console.log(chainId)
+    if ( chainId != "0x4") {
+        alert("Please change to Rinkeby/")
+    }
+    else {
+        
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
 
-          console.log("Account:", await signer.getAddress());
-          const balance = await provider.getBalance(signer.getAddress());
-          console.log("Balance: ", balance);
-          
-          return await signer.getAddress();
-      }
+        console.log("Account:", await signer.getAddress());
+        const balance = await provider.getBalance(signer.getAddress());
+        console.log("Balance: ", balance);
+        
+        return await signer.getAddress();
+    }
   }
   else{
       alert("No wallet/ethereum provider detected, please install metamask.")
