fa8096c48ac28e68e4d605e489d7abb08cf154c2
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-03-02 11:58:45 +0000

    Remove conflict markers.

--- src/app/src/lib/getaccs.ts
+++ src/app/src/lib/getaccs.ts
@@ -56,12 +56,9 @@ export async function getAccoladesByContract(address: string, contract: Contract
 
 
 export async function getAllContractAccolades(contractAddress: string) {
-<<<<<<< HEAD
-=======
 
   console.log(`contract: ${contractAddress}`);
 
->>>>>>> main
   const provider = new ethers.providers.Web3Provider(window.ethereum)
   const signer = provider.getSigner();
   const abi = [
