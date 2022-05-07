b61131dc26200cc52183f5d7755bf1bf0890cf64
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-05-07 18:34:26 +0100

    Delay accolade fetching until after page load.

--- src/app/src/pages/accolades/[accolades].tsx
+++ src/app/src/pages/accolades/[accolades].tsx
@@ -3,17 +3,14 @@ import {
   Text,
   Wrap,
   WrapItem,
-  Heading
+  Heading,
+  VStack,
+  Spinner
 } from '@chakra-ui/react'
-import { GetServerSideProps } from 'next'
-import { ethers } from 'ethers'
 
 import { Container, Main, Footer, AccoladeCard } from '../../components'
-import { getAccoladesByContract, getIdsAndTimestampsByEvents } from "../../lib/getaccs"
-import _signets from '../../lib/signets.json'
-const { signets } = _signets
-import _abi from "../../lib/abi.json";
-const { abi } = _abi
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 interface AccoladeAPIResponse {
   [key: string]: AccoladeAPIDatum[]
@@ -25,70 +22,79 @@ export interface AccoladeAPIDatum {
   description: string
 }
 
-const Accolades = ({accolades}: {accolades: AccoladeAPIResponse}) => {
-  console.log(accolades)
-  return (
-  <Container height="100vh">
-    <Head>
-      <title>My Accolades </title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Heading>My Accolades</Heading>
-    <Main>
-      {/* <Integrations/> */}
-      <Wrap spacing={{ base: 5, lg: 8 }} justify='center'>
-          {["spotify", "oxhack", "redcross", "strava"].map( data => accolades[data].map(
-            (datum: AccoladeAPIDatum, idx: number) => <WrapItem key={idx} >
-            <AccoladeCard 
-              title={datum.name} 
-              companyName={data} 
-              imageUrl={datum.image_url} 
-              description={datum.description}
-              categories={[]}
-              companyLogoUrl={''} /></WrapItem>))}
-      </Wrap>
-    </Main>
+// const Accolades = ({ accolades }: { accolades: AccoladeAPIResponse }) => {
+const Accolades = () => {
 
-    <Footer>
-      <Text>Courtesy of the best OxHack22 Team ❤️</Text>
-    </Footer>
-  </Container>
-)}
+  const router = useRouter();
+
+  const [accolades1, setAccolades1] = useState<AccoladeAPIResponse>({});
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  const { accolades: address } = router.query;
+
+  useEffect(() => {
+    console.log(`address ${address}`)
+    if (address === undefined) {
+      return;
+    }
+
+    async function fetchData() {
+      const res = await fetch('/api/fetchaccs', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ address })
+      })
+
+      res.json()
+        .then(({ accolades }) => {
+          setAccolades1(accolades)
+          setLoaded(true);
+        })
+        .catch(console.error)
+    }
+    fetchData();
+  }, [address])
+
+  console.log(loaded)
+
+  return (
+    <Container height="100vh">
+      <Head>
+        <title>My Accolades </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Heading>My Accolades</Heading>
+      <Main>
+        {/* <Integrations/> */}
+        {!loaded ? (
+          <VStack>
+            <Text fontSize='lg'>Loading</Text>
+            <br />
+            <Spinner size='xl' />
+          </VStack>
+        ) : (
+          <Wrap spacing={{ base: 5, lg: 8 }} justify='center'>
+            {["spotify", "oxhack", "redcross", "strava"].map(data => accolades1[data].map(
+              (datum: AccoladeAPIDatum, idx: number) => <WrapItem key={idx} >
+                <AccoladeCard
+                  title={datum.name}
+                  companyName={data}
+                  imageUrl={datum.image_url}
+                  description={datum.description}
+                  categories={[]}
+                  companyLogoUrl={''} /></WrapItem>))}
+          </Wrap>
+        )
+        }
+      </Main>
+
+      <Footer>
+        <Text>Courtesy of the best OxHack22 Team ❤️</Text>
+      </Footer>
+    </Container >
+  )
+}
 
 export default Accolades
-
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-
-  // return {props: {accolades :{ "spotify": [ { "name": "Lex Fridman Podcast Superfan — 2021", "image_url": "https://raw.githubusercontent.com/mbiss10/oxhack22/main/spotify_achievement_images/lex.png", "description": "The Lex Fridman Podcast was your most listened-to podcast this year!" }, { "name": "Tastemaker", "image_url": "https://raw.githubusercontent.com/mbiss10/oxhack22/main/spotify_achievement_images/taste.png", "description": "You created a playlist that amassed over 20 followers. You must have good taste!" }, { "name": "Eclectic Ears", "image_url": "https://raw.githubusercontent.com/mbiss10/oxhack22/main/spotify_achievement_images/ears.png", "description": "You listened to songs from 15 different genres this year!" } ], "oxhack": [], "redcross": [], "strava": [] }}}
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RINKEBY_URL);
-
-  const address = context.query.accolades!.toString().toLowerCase();
-  const accolades = new Object();
-
-  for (const contractAddr in signets) {
-    const contract = new ethers.Contract(
-      contractAddr,
-      abi,
-      provider,
-    );
-
-    let tokens = await getIdsAndTimestampsByEvents(address, contract);
-
-    if (tokens === null) {
-      // @ts-ignore
-      console.error(`could not get accolades for ${signets[contractAddr]}`);
-      tokens = [];
-    }
-
-    accolades[signets[contractAddr]] = tokens;
-  }
-
-  console.log(JSON.stringify(accolades, null, 2))
-
-  return {
-    props: {
-      accolades,
-    }
-  }
-}
---
+++ src/app/src/pages/api/fetchaccs.ts
@@ -0,0 +1,50 @@
+import { ethers } from "ethers";
+import { NextApiRequest, NextApiResponse } from "next";
+import _abi from "../../lib/abi.json";
+import _signets from '../../lib/signets.json'
+import { getIdsAndTimestampsByEvents } from "../../lib/getaccs";
+
+const { signets } = _signets
+const { abi } = _abi;
+const provider = new ethers.providers.JsonRpcProvider(process.env.RINKEBY_URL);
+
+export default async function FetchAccs(req: NextApiRequest, res: NextApiResponse) {
+
+  try {
+
+    const { address } = req.body;
+    let accolades = new Object();
+
+    console.log(`fetching accolades for ${address}`)
+    console.log(`signets: ${signets}`)
+
+    if (address === undefined) {
+      return res.status(400).json({ error: "400: no address provided", accolades: {} });
+    }
+
+
+    for (const contractAddress in signets) {
+      const contract = new ethers.Contract(
+        contractAddress,
+        abi,
+        provider,
+      )
+
+      let tokens = await getIdsAndTimestampsByEvents(address, contract);
+
+      if (tokens === null) {
+        // @ts-ignore
+        console.error(`could not get accolades for ${signets[contractAddr]}`);
+        tokens = [];
+      }
+
+      accolades[signets[contractAddress]] = tokens;
+    }
+
+    res.status(200).json({ accolades });
+
+  } catch {
+    res.send(500);
+  }
+
+}
