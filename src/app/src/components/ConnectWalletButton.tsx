c973bfca4d4fb8b1e6679ab92b1e6b76942d86a9
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-05-20 11:54:18 +0100

    Integrate rainbowkit

--- src/app/src/components/ConnectWalletButton.tsx
+++ src/app/src/components/ConnectWalletButton.tsx
@@ -1,17 +1,14 @@
 import { Button, Center, Image } from "@chakra-ui/react"
 import React, { useContext } from "react";
-import { getAddress } from "../utils/metamask";
-import { UserContext } from "../utils/UserContext";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 
 export const ConnectWalletButton: React.FC = () => {
-  const { user, setUser } = useContext(UserContext);
 
   return (
     <Center>
-      <Button colorScheme={user.address ? 'purple' : 'green'} type='submit' className="connectButton" onClick={user.address ? undefined : async () => setUser({ address: await getAddress(true) })} >
-        {user.address ? user.address.slice(0, 7) : 'Connect'}
-      </Button>
-    </Center>
+      <ConnectButton label="Connect" />
+    </Center >
   )
 }
